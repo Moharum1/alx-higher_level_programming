7b996535a1f37e79e4c89a361aef6c272521f649
Moharum1 <moharum4@gamil.com>
2023-11-28 20:43:29 +0200

    task done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import string
+def uppercase(str):
+    newStr = ""
+    for char in str:
+        if char in string.ascii_lowercase:
+            newStr += chr(ord(char) - 32)
+        else:
+            newStr += char
+
+    print(newStr)
