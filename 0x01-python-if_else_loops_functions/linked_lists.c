f3d4e53980d02cc1ee078582d1d42b90e7e27e3f
Moharum1 <moharum4@gamil.com>
2023-11-28 21:58:04 +0200

    task done

---
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - add a new element to a sorted node
+ * @head: pointer to head of list
+ * @number: number we want to add to the node
+ * Return: the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+	listint_t *future;
+
+	current = *head;
+	future =  current->next;
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (future != NULL)
+		{
+			if (number < future->n)
+			{
+				current->next = new;
+				new->next = future;
+				return (new);
+			}
+			future = future->next;
+			current = current->next;
+		}
+	}
+	return (new);
+}
+
