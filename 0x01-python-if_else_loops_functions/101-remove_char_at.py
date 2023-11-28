db53177f9739105e5c917d202d107f921f8480bb
Moharum1 <moharum4@gamil.com>
2023-11-28 22:58:14 +0200

    task done

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    newStr = str[ : n] + str[ n + 1 : ]
+    if n < 0:
+        return str
+
+    newStr = str[: n] + str[n + 1:]
     return newStr
