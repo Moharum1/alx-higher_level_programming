caa4f7a10a77e763ad4dda1f4302b5531e973a05
Moharum1 <moharum4@gamil.com>
2023-11-30 14:27:31 +0200

    task done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,15 @@ if __name__ == "__main__":
 
     if len(arg) == 1:
         print("0 arguments.")
-    else:
+    elif len(arg) == 2:
         arg.pop(0)
         print("{} argument:".format(len(arg)))
 
         for i, cont in enumerate(arg):
             print("{} : {}".format(i + 1, cont))
+    else:
+        arg.pop(0)
+        print("{} arguments:".format(len(arg)))
 
+        for i, cont in enumerate(arg):
+            print("{} : {}".format(i + 1, cont))
