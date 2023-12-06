8bb2806cb0d7a336e12a8de41fdddf78b8b22561
Moharum1 <moharum4@gamil.com>
2023-12-06 18:53:27 +0200

    task done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+
+    bestScore = 0 
+    name = ""
+    for key, value in a_dictionary.items():
+        if value >  bestScore:
+            bestScore = value
+            name = key
+    return name
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    newDict = {i : a_dictionary[i] * 2 for i in a_dictionary}
+    newDict = {i: a_dictionary[i] * 2 for i in a_dictionary}
     return newDict
