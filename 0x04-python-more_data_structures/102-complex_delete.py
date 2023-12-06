3daab5be9c842822e93083240d60ac6dbd4402ca
Moharum1 <moharum4@gamil.com>
2023-12-06 20:07:36 +0200

    task done

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    desiredKey = []
+
+    for key,valu in a_dictionary.items():
+        if value == valu:
+            desiredKey.append(key)
+
+    for item in desiredKey:
+        del a_dictionary[item]
+
+    return a_dictionary
