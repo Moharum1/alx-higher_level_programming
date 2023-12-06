cbc70dac7a785ff419f341086c5bb13a57b79b54
Moharum1 <moharum4@gamil.com>
2023-12-06 20:08:17 +0200

    task done

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     desiredKey = []
 
-    for key,valu in a_dictionary.items():
+    for key, valu in a_dictionary.items():
         if value == valu:
             desiredKey.append(key)
 
