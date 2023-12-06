8a154ec8583ff0e3299765fd5a3a8b7b40d020b4
Moharum1 <moharum4@gamil.com>
2023-12-06 18:55:06 +0200

    task done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
 
-    bestScore = 0 
+    bestScore = 0
     name = ""
     for key, value in a_dictionary.items():
-        if value >  bestScore:
+        if value > bestScore:
             bestScore = value
             name = key
     return name
