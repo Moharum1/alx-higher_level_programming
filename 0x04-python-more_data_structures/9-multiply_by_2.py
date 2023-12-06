167ad3c12790382800e0895fd9721dcd096fa869
Moharum1 <moharum4@gamil.com>
2023-12-06 18:46:40 +0200

    task done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,8 +2,3 @@
 def multiply_by_2(a_dictionary):
     newDict = {i : a_dictionary[i] * 2 for i in a_dictionary}
     return newDict
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print(a_dictionary)
-print("--")
-print(new_dict)
