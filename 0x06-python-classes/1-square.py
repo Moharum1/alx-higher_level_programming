fc236f214e64fa2ac41fbfc52794c3ff3e0ef162
Moharum1 <moharum4@gamil.com>
2023-12-19 15:26:32 +0200

    task done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 class Square:
+    """
+    Class that defines properties of square.
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
+        """
+        Creates new instances of square (1 side).
+        Args:
+            size: size of the square.
+        """
         self.__size = size
