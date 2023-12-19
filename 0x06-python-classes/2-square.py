cc694927aa7735026feb03942f6f79696411752b
Moharum1 <moharum4@gamil.com>
2023-12-19 15:44:52 +0200

    task done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,13 +9,13 @@ class Square:
     Attributes:
         size: size of a square (1 side).
     """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         Creates new instances of square (1 side).
         Args:
             size: size of the square.
         """
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
