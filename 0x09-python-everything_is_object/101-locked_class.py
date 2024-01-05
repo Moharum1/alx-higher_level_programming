06e6f3aeb6cf249c28ca4660533d5a543f157f9b
Moharum1 <moharum4@gamil.com>
2024-01-05 17:53:25 +0200

    task done

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class LockedClass"""
+
+
+class LockedClass:
+    """
+        LockedClass does nothing 
+    """
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instances of Locked Class."""
+
+        self.first_name = "first_name"
