f398d4031f0d7e5fa00d6da3b647db58a02ae9de
Moharum1 <moharum4@gamil.com>
2024-01-05 17:38:33 +0200

    task done

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def copy_list(list_):
-    """
-        A function used to copy a list
-
-        Args:
-            l (list): a list you want to copy
-
-        Return:
-            list: a copy of original list
-    """
     return list_[:]
