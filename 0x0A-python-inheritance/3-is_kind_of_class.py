d514420586e3a892284e430daf39e62cbfac8ac1
Moharum1 <moharum4@gamil.com>
2024-01-08 18:54:27 +0200

    task done

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""
+    A module with is_kind_of_class func
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """
+        A function to check if obj is an of a_class or obj of a sub_class of a_class
+
+        Arg:
+            obj : the object we want to check
+            a_class : the class we use for checking
+
+        Return:
+            bool : is obj of same class as a_class
+    """
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+    A module with is_kind_of_class func
+"""
+
+
 def inherits_from(obj, a_class):
+    """
+        A function to check if the object is an instance of a class 
+            that inherited (directly or indirectly) from the specified class
+
+        Arg:
+            obj : the object we want to check
+            a_class : the class we use for checking
+
+        Return:
+            bool
+    """
     return False if type(obj) is a_class else isinstance(obj, a_class)
