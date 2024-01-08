170f002779673241fd450e57204abb57387e2dde
Moharum1 <moharum4@gamil.com>
2024-01-08 19:03:55 +0200

    task done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    A module with BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """A BaseGeometry Class"""
+
+    def area(self):
+        """A function to print the area of the Geometry"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+            A function to check if value is a valid int that
+                is greater than zero
+
+            Raise:
+                TypeError : if value not integer
+                ValueError : if value <= 0
+        """
+        if isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
