0a8f8788cf8974609e37ab84f0e0bb87df474f5f
Moharum1 <moharum4@gamil.com>
2024-01-09 17:35:49 +0200

    task done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A module with one class Student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+            A function define args needed by the obj of the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrive the dic representation of class """
+        res = {}
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if attr in self.__dict__:
+                    res[attr] = self.__dict__[attr]
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+            A function that changes the value of args in obj
+        """
+        for key, value in json:
+            self.__dict__[key] = value
