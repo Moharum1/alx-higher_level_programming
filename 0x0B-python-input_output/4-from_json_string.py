c7abe8fb3b3327156e4efd079d8ea471f4ee0873
Moharum1 <moharum4@gamil.com>
2024-01-09 16:21:34 +0200

    task done

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,13 +4,5 @@ import json
 
 
 def from_json_string(my_str):
-    """
-        Convert object into json.
-
-        Args:
-            my_str (str): the string you want to convert
-
-        Return:
-            obj : the python object representation of string
-    """
-    return json.load(my_str)
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
