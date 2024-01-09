a87a86b6eba148fcb797868712b7735e8cd61d6c
Moharum1 <moharum4@gamil.com>
2024-01-09 16:25:38 +0200

    task done

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a function to save an obj file into a file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write the obj function in a certin file."""
+    with open(filename, encoding="utf-8") as file:
+        json.dump(my_obj, file)
