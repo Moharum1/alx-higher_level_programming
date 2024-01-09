cfdd308bb20b1c68b430feddce187627c57fb1ac
Moharum1 <moharum4@gamil.com>
2024-01-09 15:55:50 +0200

    task done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a text read file function"""
+
+
+def read_file(filename=""):
+    """
+        Print the contents of a UTF8 text file to stdout.
+
+        Args:
+            filename (str): a string describing the location of a file
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
