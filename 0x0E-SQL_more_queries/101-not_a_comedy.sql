55cabd2f5dd02b87ec61710f13f015efb9a5d476
Moharum1 <moharum4@gamil.com>
2024-02-15 23:14:38 +0200

    task done

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,10 @@
+
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.id NOT IN
+    (
+        SELECT tv_shows.id FROM tv_shows
+        JOIN tv_show_genres ON tv_show_genres.show_id = id
+        JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+        WHERE tv_genres.name = "Comedy"
+    )
+ORDER BY title;
