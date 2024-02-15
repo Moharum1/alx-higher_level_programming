63a52e2f55c1382fefcdef3571405490826bd176
Moharum1 <moharum4@gamil.com>
2024-02-15 21:53:33 +0200

    task done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,7 @@
 -- create a db with an auto generated id primary key
     -- forgin key which is the state id 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
-    (
-        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        state_id INT UNIQUE,
-        FOREIGN KEY(state_id) REFERENCES states(id),
-        name VARCHAR(256) NOT NULL
-    )
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
