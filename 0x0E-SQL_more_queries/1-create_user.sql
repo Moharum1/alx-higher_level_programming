fba969fd3653b21b2140c0b82ff53114155efda1
Moharum1 <moharum4@gamil.com>
2024-02-15 20:01:26 +0200

    task done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create a User on Sql SERVER and give it all permissions
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
