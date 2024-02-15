936f7b4f13c511171fa3d482afa53d7269c4d322
Moharum1 <moharum4@gamil.com>
2024-02-15 21:49:02 +0200

    task done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,5 @@
+-- create a db with an auto generated id primary key
+    -- forgin key which is the state id 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
     (
