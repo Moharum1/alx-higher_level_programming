c8ebdfcb2b174bb21fdf8ccd08aaa0835ea9bb0e
Moharum1 <moharum4@gamil.com>
2024-03-13 13:47:51 +0200

    task done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,12 @@
 
 exports.esrever = function (list) {
   const revlist = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     revlist.push(list[i]);
   }
   return revlist;
 };
+
+const esrever = require('./8-esrever').esrever;
+
+console.log(esrever(['1', 2, { id: 3 }, 4, '5', 6]));
