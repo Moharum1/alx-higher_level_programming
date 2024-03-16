5bd52a73f7146e84091743c56e73a3ac6fff57d0
Moharum1 <moharum4@gamil.com>
2024-03-17 00:31:36 +0200

    task done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A simple use of MySQLdb to display data from db
+
+    Usage ./2-my_filter_states.py <mysql username> <mysql password> <db name> <state name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    params = sys.argv
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=params[1],
+        passwd=params[2],
+        db=params[3]
+    )
+    state = params[4]
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM states WHERE name = '{}'".format(state))
+    data = cur.fetchall()
+
+    for row in data:
+        print(row)
