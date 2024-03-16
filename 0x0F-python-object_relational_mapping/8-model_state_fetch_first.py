6bb99dbe2178579573b396c7ce3cf3ffbd4b6eb5
Moharum1 <moharum4@gamil.com>
2024-03-17 01:27:15 +0200

    task done

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A simple use of sqlalchmey
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State
+import sys
+
+if __name__ == "__main__":
+    params = sys.argv
+    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                          format(params[1], params[2], params[3]),
+                          pool_pre_ping=True)
+
+    with Session(engin) as session:
+        query = session.query(State)
+        state = query.first()
+
+        print("{}: {}".format(state.id, state.name))
